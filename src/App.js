02e2f8d2002a0f606b709464df5aa2de28ff741f
shir's <shirram54@gmail.com>
2021-10-21 19:14:59 +0300

    add shop

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+import Products from './components/Products/Products';
+import React from 'react';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Products />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Products/Products.js
@@ -0,0 +1,22 @@
+import Product from "../Product/Product";
+import './Products.css';
+
+function Products() {
+    return (
+        <section class="products">
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+        </section>
+    );
+}
+
+export default Products;
+
