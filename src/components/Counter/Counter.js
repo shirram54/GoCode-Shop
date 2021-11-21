a8f6d2d5c50121150b8b36a4bad43b9c1f638c6e
Shir Ram <shirram54@gmail.com>
2021-11-21 21:26:12 +0200

    add

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,22 +1,17 @@
-import React, { useState } from 'react';
-import './Counter.css';
-
+import React, { useState } from "react";
+import "./Counter.css";
 
 function Counter() {
-    const [count, setCount] = useState(true);
+  const [count, setCount] = useState(false);
 
-    return (
-        <div className="App">
-            <button className="btn" onClick={() => setCount(!count)}>Welcome msg</button>
-            {count && <div>Welcome to GoCode Shop</div>}
-        </div>
-    );
+  return (
+    <div className="App">
+      <button className="btn" onClick={() => setCount(!count)}>
+        Welcome msg
+      </button>
+      {count && <div>Welcome to GoCode Shop</div>}
+    </div>
+  );
 }
 
-
 export default Counter;
-
-
-
-
-
